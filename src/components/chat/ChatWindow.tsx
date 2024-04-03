05db302e9061680f4bb7da6708efc313eee3cb88
Joe Swann <joe@1of1studio.com>
2024-04-04 10:10:43 +1300

    Styling and qol updates

--- src/components/chat/ChatWindow.tsx
+++ src/components/chat/ChatWindow.tsx
@@ -8,6 +8,14 @@ const ChatWindow: DefaultComponentInterface = ({ className }) => {
   const { messages } = useChat();
   return (
     <div className={cx(className, styles.container)}>
+      {messages.length === 0 && (
+        <div className={styles.message}>
+          <CommonMarkdown>
+            Welcome to Snap! You can start chatting by typing in the chat input
+            below.
+          </CommonMarkdown>
+        </div>
+      )}
       {messages.map((message) => (
         <div
           className={cx(styles.message, styles[`message--${message.role}`])}
